5476da9166317db23e6c29044c46e8bb2b8be38c
Jialiang Yuan <florentjialiang@163.com>
2025-03-01 02:39:01 -0500

    add

--- QKV.py
+++ QKV.py
@@ -1,6 +1,5 @@
 default_weight = 0.1428571428571429
-baseline_value = 24.140140533447266
-
+baseline_value =  24.140140533447266
 # 原始数值
 raw_values = [23.4, 23.72, 23.82, 23.98, 24.46, 24.37, 24.21]
 components = ["Q", "K", "V", "Output", "Gate", "Up", "Down"]
@@ -12,7 +11,7 @@ differences = [v - baseline_value for v in raw_values]
 mean_diff = sum(differences) / len(differences)
 
 # 设置缩放因子 s，保证调整后权重不会相差太大，最好使最高不超过0.15
-s = 0.011
+s = 0.013
 
 # 计算每个组件的权重
 weights = [default_weight - s * (d - mean_diff) for d in differences]
--- prune.py
+++ prune.py
@@ -539,19 +539,19 @@ def ww_sparsity_llama2_7b_split(args, model, device=torch.device("cuda:0"),
 
     for i in range(32):
         #Q
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.149394 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.152366 * 7)
         #K
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.145874 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *0.147246* 7)
         #V
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.144774 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *  0.145646 * 7)
         #OUT
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.143014 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *  0.143086 * 7)
         #GATE
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.137734 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *   0.135406 * 7)
         #UP
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.138724 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *0.136846* 7)
         #DOWN
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.140484 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.139406* 7)
 
     res = torch.tensor(res, dtype=torch.float32)
 
