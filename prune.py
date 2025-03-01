65a8c4ad4503375d3c7edb99e03958162f424cb3
Jialiang Yuan <florentjialiang@163.com>
2025-03-01 03:37:59 -0500

    add

--- prune.py
+++ prune.py
@@ -539,19 +539,19 @@ def ww_sparsity_llama2_7b_split(args, model, device=torch.device("cuda:0"),
 
     for i in range(32):
         #Q
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.147314 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *0.148206 * 7)
         #K
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.144914 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.145326 * 7)
         #V
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.144164 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.144426 * 7)
         #OUT
-        res.append(layerwise_pruning_ratios_esd[i*7] *   0.142964 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *   0.142986 * 7)
         #GATE
-        res.append(layerwise_pruning_ratios_esd[i*7] *   0.139364 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *  0.138666 * 7)
         #UP
-        res.append(layerwise_pruning_ratios_esd[i*7] *  0.140039 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] *  0.139476 * 7)
         #DOWN
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.141239 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.140916 * 7)
 
     res = torch.tensor(res, dtype=torch.float32)
     print("sum : ", res.sum() / (7*32))
