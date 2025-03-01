ddfd440709a3bb85904bf8fc1477c0ef5b685951
Jialiang Yuan <florentjialiang@163.com>
2025-02-28 23:13:50 -0500

    add

--- prune.py
+++ prune.py
@@ -539,19 +539,19 @@ def ww_sparsity_llama2_7b_split(args, model, device=torch.device("cuda:0"),
 
     for i in range(32):
         #Q
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.137 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.142 * 7)
         #K
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.138* 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.142* 7)
         #V
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.143 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.142 * 7)
         #OUT
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.144 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.142 * 7)
         #GATE
         res.append(layerwise_pruning_ratios_esd[i*7] * 0.142 * 7)
         #UP
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.146 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.145 * 7)
         #DOWN
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.150 * 7)
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.145 * 7)
 
     res = torch.tensor(res, dtype=torch.float32)
 
