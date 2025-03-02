22db6dd06b957b9d1a47940e14b399eec5da64c4
Jialiang Yuan <florentjialiang@163.com>
2025-03-01 21:47:48 -0500

    add“

--- prune.py
+++ prune.py
@@ -765,10 +765,10 @@ def ww_sparsity_llama3_8b_split(args, model, device=torch.device("cuda:0"),
         #GATE
         res.append(layerwise_pruning_ratios_esd[i*7]  * 0.142524  * 7   )
         #UP
-        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.142524 * 7  )
+        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.144857 * 7  )
         #DOWN
 
-        res.append(layerwise_pruning_ratios_esd[i*7]  * 0.144857 * 7  )
+        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.142524 * 7  )
     print(res)
     return res
     
