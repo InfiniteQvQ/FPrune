d33e52199794ca35656bb6a605971564816a30aa
Jialiang Yuan <florentjialiang@163.com>
2025-03-01 22:45:22 -0500

    add

--- prune.py
+++ prune.py
@@ -755,20 +755,20 @@ def ww_sparsity_llama3_8b_split(args, model, device=torch.device("cuda:0"),
 
     for i in range(32):
         #Q
-        res.append(layerwise_pruning_ratios_esd[i*7] * 0.151 * 7 )
+        res.append(layerwise_pruning_ratios_esd[i*7] * 0.142218 * 7 )
         #K
-        res.append(layerwise_pruning_ratios_esd[i*7]  * 0.151  * 7 )
+        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.142697 * 7 )
         #V
         res.append(layerwise_pruning_ratios_esd[i*7]  * 0.151 * 7 )
         #OUT
-        res.append(layerwise_pruning_ratios_esd[i*7]  * 0.151 * 7  )
+        res.append(layerwise_pruning_ratios_esd[i*7]  * 0.142218 * 7  )
         #GATE
-        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.094 * 7   )
+        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.140122 * 7   )
         #UP
-        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.151 * 7  )
+        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.141622 * 7  )
         #DOWN
 
-        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.151 * 7  )
+        res.append(layerwise_pruning_ratios_esd[i*7]  *  0.140122 * 7  )
     print(res)
     return res
     
