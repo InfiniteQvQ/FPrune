e6eaa29c095f68645f8a590c1abf23c124e43ed3
Jialiang Yuan <florentjialiang@163.com>
2025-03-09 22:29:17 -0400

    add

--- Vib.py
+++ Vib.py
@@ -95,7 +95,7 @@ def get_dataloader():
     cache_dir = "/root/autodl-tmp"  # 🚀 指定缓存目录
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B", cache_dir=cache_dir)
     
-    dataset = load_dataset("openwebtext", split="train", cache_dir=cache_dir)  # 🚀 下载数据到 `/root/autodl-tmp`
+    dataset = load_dataset("ptb_text_only", "penn_treebank", split="train", cache_dir=cache_dir)  # 🚀 下载数据到 `/root/autodl-tmp`
 
     def tokenize_function(examples):
         return tokenizer(examples["text"], truncation=True, padding="max_length", max_length=512)
