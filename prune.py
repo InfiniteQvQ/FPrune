9abff4d207f7f1fbf4540ed97b99c7baed5b1720
Jialiang Yuan <florentjialiang@163.com>
2025-03-13 17:45:56 -0400

    add

--- prune.py
+++ prune.py
@@ -999,7 +999,7 @@ def ww_sparsity_test_3b(args, model, device=torch.device("cuda:0"),
     layerwise_pruning_ratios_esd = layerwise_pruning_ratios_esd * scaler
     layerwise_pruning_ratios_esd = layerwise_pruning_ratios_esd.cpu().numpy().tolist()
     print("ESD-based ratios:", layerwise_pruning_ratios_esd)
-
+    return layerwise_pruning_ratios_esd
     segments = {
         0: [0],
         1: [1],
